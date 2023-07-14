e7345fe464eb3f0fdb7db5574546791f30e815d3
reogman <mantracode@yahoo.com>
2023-07-14 18:31:33 +0700

    modif Children model

--- src/fetcher_models/category.rs
+++ src/fetcher_models/category.rs
@@ -17,12 +17,5 @@ pub struct Parent {
 pub struct Children {
     pub name: String,
     pub url: String,
-    pub children: Vec<Children2>,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Children2 {
-    pub name: String,
-    pub url: String,
+    pub children: Option<Vec<Children>>,
 }
