6f1e08edecbff01e7a3d303c7226962c02e96041
mantracode <mantr4code@gmail.com>
2023-07-19 07:46:43 +0700

    💻 reexport reqwest

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,6 @@ pub use fetchers::client_categories::*;
 pub use fetchers::client_category_page_count::*;
 pub use fetchers::client_products_at_page::*;
 pub use fetchers::client_single_product::*;
+
+pub use reqwest;
+pub use reqwest::Client;
